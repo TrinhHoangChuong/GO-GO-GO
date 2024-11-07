21d67c7e11b43e52f691b37bca385afbe31c11b1
Phan Hoàng Hiệp <phanhoanghiep13502k5@gmail.com>
2024-11-07 08:19:42 +0700

    Add files via upload

---
+++ Feedback.cs
@@ -0,0 +1,16 @@
+﻿namespace Libs
+{
+    public class Feedback
+    {
+        public string FeedbackID { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+
+        public Feedback(string feedbackID, int rating, string comment)
+        {
+            FeedbackID = feedbackID;
+            Rating = rating;
+            Comment = comment;
+        }
+    }
+}
